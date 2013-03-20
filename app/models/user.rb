be229ad30a2c3f021685717f67cbee3c0aa98830
dougfarre <dougfarre@gmail.com>
2013-03-20 05:15:28 +0000

    added icons and started on dashboard

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable,
   # :lockable, :timeoutable and :omniauthable
-  devise :invitable, :database_authenticatable, :registerable, :confirmable,
+  devise :invitable, :database_authenticatable, :registerable, #:confirmable,
          :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
@@ -11,13 +11,20 @@ class User < ActiveRecord::Base
   attr_accessible :name, :email, :password, :password_confirmation, :remember_me
   attr_accessible :image_id
  
-  has_many :items #
+  has_many :items
   has_many :user_addresses
-  has_many :addresses, :through => :user_addresses #
+  has_many :addresses, :through => :user_addresses
   has_many :user_meeting_locations
-  has_many :meeting_locations, :through => :user_meeting_locations #
+  has_many :meeting_locations, :through => :user_meeting_locations
 
-  has_one :preference #
+  has_one :preference
 
-  belongs_to :image #
+  belongs_to :image 
+
+  after_create :create_preference
+
+  def create_preference
+    self.build_preference 
+    self.save
+  end
 end
